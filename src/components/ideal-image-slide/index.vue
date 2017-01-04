2d5b92b78c4e3d97ed36e7ed4613de9a120096f0
YAOHAO <yaohao@secondcompany.cn>
2017-01-04 21:47:36 +0800

    single image detail bug

--- src/components/ideal-image-slide/index.vue
+++ src/components/ideal-image-slide/index.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="slider">
-    <img :src="url" v-for="url in urls">
+    <img :src="url" v-for="url in urls" @load="urls.length <= 1 && imgLoad()">
   </div>
 </template>
 
 <script>
-import IdealImageSlider from "./ideal-image-slider.js"
-import Short from "../../assets/images/blog/short.png"
-import Long from "../../assets/images/blog/long.jpg"
-export default {
-  props: {
-    urls: {
-      type: Array,
-      default: function(){
-        return [Short,Long]
+  import IdealImageSlider from "./ideal-image-slider.js"
+  import Short from "../../assets/images/blog/short.png"
+  import Long from "../../assets/images/blog/long.jpg"
+  export default {
+    props: {
+      urls: {
+        type: Array,
+        default: function () {
+          return [Short, Long]
+        }
+      }
+    },
+    ready() {
+      new IdealImageSlider.Slider('#slider');
+    },
+    methods: {
+      imgLoad: function () {
+        var slider = document.getElementById("slider")
+        slider.style.height = slider.children[0].offsetHeight + 'px'
       }
     }
-  },
-  ready () {
-    new IdealImageSlider.Slider('#slider');
   }
-}
 </script>
 <style>
 @import "./ideal-image-slider.css";
