63bae35eaaa3b71022b258b142b1a9a957e4f64b
YAOHAO <yaohao@secondcompany.cn>
2017-01-15 19:32:32 +0800

    models create

--- config/models.js
+++ config/models.js
@@ -10,7 +10,7 @@
  */
 
 module.exports.models = {
-  migrate: 'safe',
+  migrate: 'create',
   connection: 'someMysqlServer'
   
   /***************************************************************************
