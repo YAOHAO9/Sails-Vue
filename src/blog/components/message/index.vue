f6f32ef98d078c861dbfc6f2f268e539750e0cb3
YAOHAO <yaohao@secondcompany.cn>
2017-02-06 23:09:18 +0800

    message update

--- src/blog/components/message/index.vue
+++ src/blog/components/message/index.vue
@@ -5,15 +5,28 @@
             <p class="time">
                 <span>{{ item.date | time }}</span>
             </p>
-            <div class="main" :class="{ self: item.self }">
-                <img class="avatar" width="30" height="30" :src="!item.self ? '/static/img/mm.jpg': '/static/img/widget_dface.444bbfa.png'" />
-                <div class="text">{{ item.content }}</div>
+            
+            <div class="hiv" :class="{ self: item.self }">
+                <div class="head" v-if="!item.self">
+                    <avator  :avatar="/static/img/mm.jpg"></avator>
+                </div>
+                <div class="content">
+                    <div class="name">刀郎</div>
+                    <div class="text">
+                    {{ item.content }}
+                    </div>
+                </div>
+                <div class="head" v-if="item.self">
+                    <avator  :avatar="/static/img/mm.jpg"></avator>
+                </div>
             </div>
         </li>
     </ul>
 </div>
 </template>
 <script>
+import Avator from '../avatar'
+
 export default  {
     data() {
       return {
@@ -24,23 +37,35 @@ export default  {
         messages: 
           {
             id: 1,
-            user: {
+            messages: [
+              {
+                  user: {
               name: '示例介绍',
               img: '/static/img/mm.jpg'
             },
-            messages: [
-              {
                 content: 'Hello，这是一个基于Vue + Vuex + Webpack构建的简单chat示例，聊天记录保存在localStorge, 有什么问题可以通过Github Issue问我。',
                 date: new Date()
               }, {
+                  user: {
+              name: '示例介绍',
+              img: '/static/img/mm.jpg'
+            },
                 content: '项目地址: https://github.com/coffcer/vue-chat',
                 date: new Date()
               },
               {
+                  user: {
+              name: '示例介绍',
+              img: '/static/img/mm.jpg'
+            },
                 content: 'Hello，这是一个基于Vue + Vuex + Webpack构建的简单chat示例，聊天记录保存在localStorge, 有什么问题可以通过Github Issue问我。',
                 date: new Date(),
                 self:true
               }, {
+                  user: {
+              name: '示例介绍',
+              img: '/static/img/mm.jpg'
+            },
                 content: '项目地址: https://github.com/coffcer/vue-chat',
                 date: new Date(),
                 self:true
@@ -48,6 +73,9 @@ export default  {
             ]
           }
       }
+    },
+    components:{
+        Avator
     },
     filters: {
         // 将日期过滤为 hour:minutes
@@ -69,8 +97,10 @@ export default  {
 }
 </script>
 <style lang="less" scoped>
+
+
 .message {
-    padding: 10px 15px;
+    padding: 10px 5px;
     overflow-y: scroll;
 
     li {
@@ -89,42 +119,41 @@ export default  {
             background-color: #dcdcdc;
         }
     }
-    .avatar {
-        float: left;
-        margin: 0 10px 0 0;
-        border-radius: 3px;
-        width: 30px;
+    .head{
+        width: 46px;
     }
-    .text {
-        display: inline-block;
-        position: relative;
-        padding: 0 10px;
-        max-width: ~'calc(100% - 40px)';
-        min-height: 30px;
-        line-height: 2.5;
-        font-size: 12px;
-        text-align: left;
-        word-break: break-all;
-        background-color: #fafafa;
-        border-radius: 4px;
+    .content{
+        width: 100%;
+        margin: 0 5px;
 
-        &:before {
-            content: " ";
-            position: absolute;
-            top: 9px;
-            right: 100%;
-            border: 6px solid transparent;
-            border-right-color: #fafafa;
+        .name{
+            font-size: 12px;
+        }
+        .text {
+            display: inline-block;
+            position: relative;
+            padding: 0 10px;
+            min-height: 30px;
+            line-height: 2.5;
+            font-size: 12px;
+            text-align: left;
+            word-break: break-all;
+            background-color: #fafafa;
+            border-radius: 4px;
+
+            &:before {
+                content: " ";
+                position: absolute;
+                top: 9px;
+                right: 100%;
+                border: 6px solid transparent;
+                border-right-color: #fafafa;
+            }
         }
     }
 
     .self {
         text-align: right;
-
-        .avatar {
-            float: right;
-            margin: 0 0 0 10px;
-        }
         .text {
             background-color: #b2e281;
 
