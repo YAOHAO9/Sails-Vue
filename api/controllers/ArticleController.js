bc977f815784ef7f616b650e2821b39bdb1753b5
YAOHAO <yaohao@secondcompany.cn>
2018-07-22 22:27:06 +0800

    add article from segmentfault

--- api/controllers/ArticleController.js
+++ api/controllers/ArticleController.js
@@ -4,117 +4,79 @@
  * @description :: Server-side logic for managing articles
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-'use strict'
-var UploadService = require("../services/UploadService")
-let fs = require('fs')
-let path = require('path')
-let uuid = require('node-uuid');
-let cheerio = require('cheerio')
-let request = require('request')
-let md5 = require('md5')
+"use strict";
+let UploadService = require("../services/UploadService");
+let fs = require("fs");
+let path = require("path");
+let cheerio = require("cheerio");
+let request = require("request");
+let md5 = require("md5");
+let parseImgSrc = require("../services/ParseImgSrc");
+let markdown = require("markdown-it")();
+let superagent = require("superagent");
+let Promise = require("bluebird");
 
-let imgSrc = (content) => {
-  let $ = cheerio.load(content)
-  let imgs = $('img')
-  let tasks = []
-  for (let i = 0; i < imgs.length; i++) {
-    let img = imgs[i]
-    let src = img.attribs.src
-    if (src.indexOf('http') < 0)
-      continue
-    let task = new Promise((resolve, reject) => {
-      let fileFd = path.join('.tmp', 'uploads', md5(src))
-      let writeStream = fs.createWriteStream(fileFd)
-      request(src).pipe(writeStream)
-      writeStream.on('finish', function () {
-        resolve(fileFd)
-      })
-      writeStream.on('error', function (err) {
-        reject(err)
-      })
+let bgImgUrl = content => {
+  let urls = content.match(/url\([^\)]*\)/g);
+  if (!urls) urls = [];
+  urls = urls.map(url => {
+    return url
+      .replace("url(", "")
+      .replace(")", "")
+      .replace(/&quot;/g, "");
+  });
+  let tasks = urls.map(url => {
+    if (url.indexOf("http") < 0) return;
+    return new Promise((resolve, reject) => {
+      let fileFd = path.join(".tmp", "uploads", md5(url));
+      let writeStream = fs.createWriteStream(fileFd);
+      request(url).pipe(writeStream);
+      writeStream.on("finish", function() {
+        resolve(fileFd);
+      });
+      writeStream.on("error", function(err) {
+        reject(err);
+      });
     })
       .then(fileFd => {
         return File.create({
           fd: fileFd,
-          type: 'image/jpeg',
-          filename: src,
-          size: 0
-        })
-      })
-      .then(file => {
-        $(img).attr('src', 'api/file/find/' + file.id)
-      })
-    tasks.push(task)
-  }
-  return Promise.all(tasks)
-    .then(() => {
-      return $.html()
-    })
-}
-let bgImgUrl = (content) => {
-  let urls = content.match(/url\([^\)]*\)/g)
-  if (!urls)
-    urls = []
-  urls = urls.map(url => {
-    return url.replace('url(', '').replace(')', '').replace(/&quot;/g, '')
-  })
-  let tasks = urls.map((url) => {
-    if (url.indexOf('http') < 0)
-      return
-    return new Promise((resolve, reject) => {
-      let fileFd = path.join('.tmp', 'uploads', md5(url))
-      let writeStream = fs.createWriteStream(fileFd)
-      request(url).pipe(writeStream)
-      writeStream.on('finish', function () {
-        resolve(fileFd)
-      })
-      writeStream.on('error', function (err) {
-        reject(err)
-      })
-    })
-      .then((fileFd) => {
-        return File.create({
-          fd: fileFd,
-          type: 'image/jpeg',
+          type: "image/jpeg",
           filename: url,
           size: 0
-        })
+        });
       })
       .then(file => {
-        content = content.replace(url, 'api/file/find/' + file.id)
-      })
-  })
-  return Promise.all(tasks)
-    .then(() => {
-      return content
-    })
-}
+        content = content.replace(url, "api/file/find/" + file.id);
+      });
+  });
+  return Promise.all(tasks).then(() => {
+    return content;
+  });
+};
 
 module.exports = {
-
-  create: function (req, res) {
+  create: function(req, res) {
     if (!req.body.title) {
-      return res.forbidden('标题不能为空')
+      return res.forbidden("标题不能为空");
     }
     if (!req.body.description) {
-      return res.forbidden('无有效内容')
+      return res.forbidden("无有效内容");
     }
-    if (!req.body.content)
-      return res.forbidden('内容不能为空')
-    return imgSrc(req.body.content)
+    if (!req.body.content) return res.forbidden("内容不能为空");
+    return parseImgSrc(req.body.content)
       .then(content => {
-        return bgImgUrl(content)
+        return bgImgUrl(content);
       })
       .then(content => {
-        return Promise.all([ArticleContent.create({ content: content }),
-        UploadService(req, "icon")
-          .then(icon => {
-            if (icon && icon.length > 0)
-              icon = icon[0]
-            else
-              icon = undefined
-            return icon
-          })])
+        return Promise.all([
+          ArticleContent.create({ content: content }),
+          UploadService(req, "icon").then(icon => {
+            if (icon && icon.length > 0) icon = icon[0];
+            else icon = undefined;
+            return icon;
+          })
+        ]);
       })
       .spread((content, icon) => {
         return Article.create({
@@ -123,67 +85,108 @@ module.exports = {
           description: req.body.description,
           content: content,
           icon: icon
-        })
+        });
       })
       .then(article => {
-        res.json(article)
+        res.json(article);
       })
       .catch(e => {
-        console.error(e)
-        res.serverError()
-      })
+        console.error(e);
+        res.serverError();
+      });
   },
-  update: function () {
+  seekArticle: function(req, res) {
+    const getHtmlByRequest = request => {
+      return new Promise((resolve, reject) => {
+        request.end((e, res) => {
+          if (e) {
+            reject(e);
+          }
+          resolve(res.text);
+        });
+      });
+    };
+    let request = superagent
+      .get("https://segmentfault.com/a/1190000009651765/edit")
+      .set("cookie", "sf_remember=88e867590f7f5f0890a4c0645e4db83e");
 
+    getHtmlByRequest(request)
+      .then(html => {
+        let $ = cheerio.load(html);
+        return parseImgSrc(
+          markdown.render($("#myEditor")[0].firstChild.data),
+          "https://segmentfault.com"
+        );
+      })
+      .then(html => {
+        let link =
+          '<link rel="stylesheet" href="/css/markdown.css" type="text/css">\n';
+        return ArticleContent.create({ content: link + html });
+      })
+      .then(content => {
+        console.log(content);
+        return Article.create({
+          user: 1,
+          title: "Article",
+          description: "Article descript",
+          content: content,
+          icon: 3
+        });
+      })
+      .then(article => {
+        res.json(article);
+      })
+      .catch(e => {
+        console.error(e);
+        res.serverError();
+      });
   },
-  find: function (req, res) {
-    if (req.param('id')) {
-      return Article.findOne(req.param('id'))
-        .populate('user')
-        .populate('content')
-        .populate('comments')
+  update: function() {},
+  find: function(req, res) {
+    if (req.param("id")) {
+      return Article.findOne(req.param("id"))
+        .populate("user")
+        .populate("content")
+        .populate("comments")
         .then(article => {
-          if (!article)
-            return res.notFound('Article not found')
-          article.content = article.content.content
-          res.json(article)
+          if (!article) return res.notFound("Article not found");
+          article.content = article.content.content;
+          res.json(article);
         })
         .catch(e => {
-          res.serverError(e.message)
-        })
+          res.serverError(e.message);
+        });
     }
-    let conds = {}
-    conds.skip = req.query.skip ? req.query.skip : '0'
-    conds.limit = req.query.limit ? req.query.limit : '10'
-    conds.sort = req.query.sort ? req.query.sort : 'createdAt DESC'
+    let conds = {};
+    conds.skip = req.query.skip ? req.query.skip : "0";
+    conds.limit = req.query.limit ? req.query.limit : "10";
+    conds.sort = req.query.sort ? req.query.sort : "createdAt DESC";
     Article.find(conds)
-      .populate('user')
-      .populate('content')
-      .populate('comments')
+      .populate("user")
+      .populate("content")
+      .populate("comments")
       .then(articles => {
-        res.ok(articles)
+        res.ok(articles);
       })
       .catch(e => {
-        res.serverError(e.message)
-      })
+        res.serverError(e.message);
+      });
   },
-  findOne: function (req, res) {
-    Article.findOne(req.param('id'))
-      .populate('user')
-      .populate('content')
-      .populate('comments')
+  findOne: function(req, res) {
+    Article.findOne(req.param("id"))
+      .populate("user")
+      .populate("content")
+      .populate("comments")
       .then(article => {
         if (!article) {
-          return res.notFound()
+          return res.notFound();
         }
-        return res.view('article', {
+        return res.view("article", {
           content: article.content.content
-        })
+        });
       })
       .catch(e => {
-        res.serverError(e.message)
-      })
-
+        res.serverError(e.message);
+      });
   }
-
-}
+};
---
+++ api/services/ParseImgSrc.js
@@ -0,0 +1,51 @@
+let fs = require('fs')
+let path = require('path')
+let cheerio = require('cheerio')
+let request = require('request')
+let md5 = require('md5')
+
+let ParseImgSrc = (content, baseHttp) => {
+  let $ = cheerio.load(content)
+  let imgs = $('img')
+  let tasks = []
+  for (let i = 0; i < imgs.length; i++) {
+    let img = imgs[i]
+    let src = img.attribs.src
+    if (src.indexOf('http') < 0) {
+      if (baseHttp && src.indexOf('/') !== -1){
+        src = baseHttp + src;
+      }else{
+        continue;
+      }
+    }
+    let task = new Promise((resolve, reject) => {
+      let fileFd = path.join('.tmp', 'uploads', md5(src))
+      let writeStream = fs.createWriteStream(fileFd)
+      request(src).pipe(writeStream)
+      writeStream.on('finish', function () {
+        resolve(fileFd)
+      })
+      writeStream.on('error', function (err) {
+        reject(err)
+      })
+    })
+      .then(fileFd => {
+        return File.create({
+          fd: fileFd,
+          type: 'image/jpeg',
+          filename: src,
+          size: 0
+        })
+      })
+      .then(file => {
+        $(img).attr('src', 'api/file/find/' + file.id)
+      })
+    tasks.push(task)
+  }
+  return Promise.all(tasks)
+    .then(() => {
+      return $.html()
+    })
+}
+
+module.exports = ParseImgSrc;
